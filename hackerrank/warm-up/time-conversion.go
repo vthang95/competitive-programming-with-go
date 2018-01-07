c7167cf2406e3508b91ee05d91aae11d0c06763e
vthang95 <tranvietthang95@gmail.com>
2018-01-08 01:05:08 +0700

    Optimize time conversion

--- hackerrank/warm-up/time-conversion.go
+++ hackerrank/warm-up/time-conversion.go
@@ -10,21 +10,15 @@ func main() {
 	var time string
 	var list []string
 	fmt.Scan(&time)
-	if time == "12:00:00AM" {
-		fmt.Println("00:00:00")
-	} else if time == "12:00:00PM" {
-		fmt.Println("12:00:00")
-	} else {
-		timePostfix := string(time[(len(time) - 2):(len(time))])
-		list = strings.Split(time, ":")
-		if timePostfix == "PM" && list[0] != "12" {
-			num, _ := strconv.Atoi(list[0])
-			list[0] = strconv.Itoa(num + 12)
-		}
-		if timePostfix == "AM" && list[0] == "12" {
-			list[0] = "00"
-		}
-		time = strings.Join(list, ":")
-		fmt.Println(string(time[0:(len(time) - 2)]))
+	timePostfix := string(time[(len(time) - 2):(len(time))])
+	list = strings.Split(time, ":")
+	if timePostfix == "PM" && list[0] != "12" {
+		num, _ := strconv.Atoi(list[0])
+		list[0] = strconv.Itoa(num + 12)
 	}
+	if timePostfix == "AM" && list[0] == "12" {
+		list[0] = "00"
+	}
+	time = strings.Join(list, ":")
+	fmt.Println(string(time[0:(len(time) - 2)]))
 }
